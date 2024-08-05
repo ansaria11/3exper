520dd141684301dc5bc1e7e05600787e2aa95fc2
ansaria11 <144002852+ansaria11@users.noreply.github.com>
2024-08-05 15:08:37 +0100

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,12 @@
-// JavaScript source code
 import Layout from "@/layout";
 import Blog from "@/components/Blog";
+import AuditedBlog from "@/components/AuditedBlog";
 
 export default function app() {
   return (
     <>
       <Layout>
-        <Blog/>
+        <AuditedBlog/>
       </Layout>
     </>
   );
